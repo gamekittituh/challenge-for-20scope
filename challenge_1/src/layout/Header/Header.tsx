48e72a87d3e671ecffa8337a8786a2968baad414
gamekittituh <gcommax@gmail.com>
2022-05-24 01:35:13 +0700

    Update Header.tsx

--- challenge_1/src/layout/Header/Header.tsx
+++ challenge_1/src/layout/Header/Header.tsx
@@ -50,6 +50,19 @@ class Header extends React.Component {
                 </div>
             </nav>
         </header>
+        // <nav className="sticky top-0 z-10 bg-white backdrop-filter backdrop-blur-lg bg-opacity-30">
+        // <div className="max-w-5xl mx-auto px-4">
+        //     <div className="flex items-center justify-between h-16">
+        //     <span className="text-2xl text-gray-900 font-semibold">Logo</span>
+        //     <div className="flex space-x-4 text-gray-900">
+        //         <a href="#">Dashboard</a>
+        //         <a href="#">About</a>
+        //         <a href="#">Projects</a>
+        //         <a href="#">Contact</a>
+        //     </div>
+        //     </div>
+        // </div>
+        // </nav>
     )
   }
 }
